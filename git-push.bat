86feabba1df57198852f918bf6dcfd9ee32a52e8
coco <308695699@qq.com>
2016-06-02 21:08:21 +0800

    update

--- git-push.bat
+++ git-push.bat
@@ -2,7 +2,7 @@ echo gitPushBat
 cd curveJS
 git init
 git remote rm origin
-git remote add origin git@git@github.com:chokcoco/FJC.git
+git remote add origin git@github.com:chokcoco/FJC.git
 git add .
 git commit -m "update"
 git push origin master
